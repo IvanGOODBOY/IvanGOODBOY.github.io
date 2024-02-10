baa815f3c60396b7036f8c0e589181539ea5b528
IvanGOODBOY <149617432+IvanGOODBOY@users.noreply.github.com>
2024-02-10 03:27:44 +0300

    Add files via upload

---
+++ js/owl_js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        
+        nav:true,
+        items:1,
+        navText:[
+            "<img src='img/feedback/arrows/left.svg'>", "<img src='img/feedback/arrows/right.svg'>"
+        ],
+        autoplay:false,
+        dots:false
+        
+    })
+    
+  });
